53415fa98b35bfd4abe3508029ddaa33cfc37de5
Unal <unaltugrulbulut@gmail.com>
2022-10-25 17:35:29 +0300

    User is landing to home page...

--- src/test/java/tests/base/TestBase.java
+++ src/test/java/tests/base/TestBase.java
@@ -5,6 +5,8 @@ import org.apache.log4j.Logger;
 import org.apache.log4j.xml.DOMConfigurator;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
@@ -30,11 +32,18 @@ public class TestBase {
     @BeforeMethod
     public void setupMethod() {
         logger.info(new Object(){}.getClass().getEnclosingMethod().getName() + ": new case is starting");
+        ChromeOptions options = new ChromeOptions();
+        //options.addArguments("--incognito");
+        options.addArguments("--disable-blink-features=AutomationControlled");
+        options.addArguments("--disable-notifications");
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        //User navigate to Web Page
+        logger.info("User is landing to home page...");
         driver.get("https://www.hepsiburada.com/");
+        
     }
 
     @AfterMethod
