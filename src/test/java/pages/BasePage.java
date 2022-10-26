42e1383896d4d2171edb42b0db95e7ed25491faa
Unal <unaltugrulbulut@gmail.com>
2022-10-26 09:19:37 +0300

    created base bage

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -1,5 +1,20 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
 public class BasePage {
+    //I am going to add all common elements on the web page
+    //constructor to initialize with PageFactory Class
+    public BasePage(){
+       PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    @FindBy()
+    public WebElement
+
+
 
 }
