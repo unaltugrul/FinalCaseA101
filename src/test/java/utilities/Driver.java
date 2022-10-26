78dde2b31c5a06639420d588ab8fd44418aa0db1
Unal <unaltugrulbulut@gmail.com>
2022-10-26 11:45:16 +0300

    I changed my Driver utility class to sigleton design because of I am going to implement page object modal

--- src/test/java/utilities/Driver.java
+++ src/test/java/utilities/Driver.java
@@ -10,25 +10,50 @@ import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
 public class Driver {
-    //I used static Webdriver because i am gonna add this attribute to static method
-    static WebDriver driver;
+    //this class singleton design pattern
 
     /*
-    I create getDrive method just for clean code, to be able to call with class name this method I used static
+    We make WebDriver private, because we want to close access from outside the class
+    We make it static because we will use it in a static method.
+    */
+    private static WebDriver driver;
+
+    /*
+        Creating a private constructor so we are closing access
+        to the object of this class from outside the class
+     */
+
+    private Driver() {
+    }
+
+    /*
+    Create a re-usable utility method which will return same driver instance when we call it
      */
     public static WebDriver getDriver() {
-        //I made such an adjustment because this website does not allow me to login
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--disable-blink-features=AutomationControlled");
-        options.addArguments("--disable-notifications");
-        //Setup chrome driver
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver(options);
-        //maximize window because some elements design might be change according to window dimension
-        driver.manage().window().maximize();
-        //implicitly wait for the element that is appearing on the web page
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        if (driver == null) {
+
+            //I made such an adjustment because this website does not allow me to login
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--disable-blink-features=AutomationControlled");
+            options.addArguments("--disable-notifications");
+            //Setup chrome driver
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver(options);
+            //maximize window because some elements design might be change according to window dimension
+            driver.manage().window().maximize();
+            //implicitly wait for the element that is appearing on the web page
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        }
         return driver;
     }
 
+    /*
+    This method will make sure our driver value is always null after using quit() method
+     */
+    public static void closeDriver() {
+        if (driver != null) {
+            driver.quit();
+            driver = null;
+        }
+    }
 }
