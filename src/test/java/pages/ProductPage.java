e4f4329538786098076694f6fb404cf239406e6c
Unal <unaltugrulbulut@gmail.com>
2022-10-27 10:34:37 +0300

    add to chart buttons added to ProductPage class

---
+++ src/test/java/pages/ProductPage.java
@@ -0,0 +1,19 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+import java.util.List;
+
+public class ProductPage {
+    public ProductPage(){
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    //"sepete ekle" buttons for every seller
+    @FindBy(xpath = "//button[@class='add-to-basket button small']")
+    public List<WebElement> sellerAddButtons;
+
+}
