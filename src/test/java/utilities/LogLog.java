38691274ae622b600fdcb84f559612b75f376e19
Unal <unaltugrulbulut@gmail.com>
2022-10-28 19:50:36 +0300

    I created different logger both two test cases, to be able to understand more clear log file

--- src/test/java/utilities/LogLog.java
+++ src/test/java/utilities/LogLog.java
@@ -3,22 +3,32 @@ package utilities;
 import org.apache.log4j.Logger;
 import org.apache.log4j.xml.DOMConfigurator;
 import tests.testCases.TestCase1;
+import tests.testCases.TestCase2;
 
 public class LogLog {
 
     //I used logger for each class because to log class name to log file
-    static Logger logger = Logger.getLogger(TestCase1.class);
+    static Logger logger1 = Logger.getLogger(TestCase1.class);
+    static Logger logger2 = Logger.getLogger(TestCase2.class);
 
     //This constructor to configure log4j.xml file
     public LogLog() {
         DOMConfigurator.configure("log4j.xml");
     }
 
-    public static void info(String message){
-        logger.info(message);
+    public static void info1(String message){
+        logger1.info(message);
     }
 
-    public static void error(String message){
-        logger.error(message);
+    public static void error1(String message){
+        logger1.error(message);
+    }
+
+    public static void info2(String message){
+        logger2.info(message);
+    }
+
+    public static void error2(String message){
+        logger2.info(message);
     }
 }
