4a88bf7bbd41f95d93e340167b67d9793860490e
Unal <unaltugrulbulut@gmail.com>
2022-10-28 21:35:08 +0300

    deleted reduntant static return type

--- src/test/java/utilities/LogLog.java
+++ src/test/java/utilities/LogLog.java
@@ -8,27 +8,27 @@ import tests.testCases.TestCase2;
 public class LogLog {
 
     //I used logger for each class because to log class name to log file
-    static Logger logger1 = Logger.getLogger(TestCase1.class);
-    static Logger logger2 = Logger.getLogger(TestCase2.class);
+     Logger logger1 = Logger.getLogger(TestCase1.class);
+     Logger logger2 = Logger.getLogger(TestCase2.class);
 
     //This constructor to configure log4j.xml file
     public LogLog() {
         DOMConfigurator.configure("log4j.xml");
     }
 
-    public static void info1(String message){
+    public void info1(String message){
         logger1.info(message);
     }
 
-    public static void error1(String message){
+    public void error1(String message){
         logger1.error(message);
     }
 
-    public static void info2(String message){
+    public void info2(String message){
         logger2.info(message);
     }
 
-    public static void error2(String message){
+    public void error2(String message){
         logger2.info(message);
     }
 }
