1ac52ca16d8efbeba288fdb760e26b496648855f
Unal <unaltugrulbulut@gmail.com>
2022-10-28 21:58:24 +0300

    inherited to BasePage.java

--- src/test/java/pages/ProductPage.java
+++ src/test/java/pages/ProductPage.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.support.PageFactory;
 
 import java.util.List;
 
-public class ProductPage {
+public class ProductPage extends BasePage{
     WebDriver driver;
 
     public ProductPage(WebDriver driver){
