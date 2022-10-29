3ad00a53c2375b00e25d1065004e41f425a20997
Unal <unaltugrulbulut@gmail.com>
2022-10-29 13:30:08 +0300

    created object for listener class

--- src/test/java/utilities/LogLog.java
+++ src/test/java/utilities/LogLog.java
@@ -7,7 +7,8 @@ import tests.testCases.TestCase2;
 
 public class LogLog {
 
-    //I used logger for each class because to log class name to log file
+    //I used logger for each class because to log class name clearly to log file
+    Logger loggerForListener = Logger.getLogger(ListenerClass.class);
      Logger logger1 = Logger.getLogger(TestCase1.class);
      Logger logger2 = Logger.getLogger(TestCase2.class);
 
@@ -16,6 +17,17 @@ public class LogLog {
         DOMConfigurator.configure("log4j.xml");
     }
 
+    //--------------> for listener class
+    public void info(String message){
+        loggerForListener.info(message);
+    }
+
+    public void error(String message){
+        loggerForListener.error(message);
+    }
+    //---------------------
+
+    //--------------> for TestCase1 Class
     public void info1(String message){
         logger1.info(message);
     }
@@ -23,6 +35,9 @@ public class LogLog {
     public void error1(String message){
         logger1.error(message);
     }
+    //---------------------
+
+    //--------------> for TestCase2 Class
 
     public void info2(String message){
         logger2.info(message);
@@ -31,4 +46,6 @@ public class LogLog {
     public void error2(String message){
         logger2.info(message);
     }
+    //---------------------
+
 }
